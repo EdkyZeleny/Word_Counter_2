e13dd518af87c1173afe6cf7c1e8b9ac609f456a
EdkyZeleny <49240143+EdkyZeleny@users.noreply.github.com>
2019-12-07 17:07:04 +0500

    Update main.cc

--- src/bin/main.cc
+++ src/bin/main.cc
@@ -7,7 +7,7 @@ using template_repository::words_checker;
 
 int main() {
   setlocale(LC_CTYPE, "rus");
-  
+
   std::string path;
   std::cout << "please, input there file source:" << std::endl;
   std::cin >> path;
@@ -15,24 +15,25 @@ int main() {
   std::ifstream file_stream;
   file_stream.open(path);
 
-  if (!pathfinder(path)){
+  if (!pathfinder(path)) {
     std::cout << "cannot open file. try another path." << std::endl;
   };
 
   std::map<std::string, size_t> word_to_count;
 
   std::string word;
-  
+
   while (file_stream >> word) {
     ++word_to_count[word];
   }
 
-  if (!words_checker(word_to_count)){
-    std::cout<< "file is empty of words"<<std::endl;
-    ;}
+  if (!words_checker(word_to_count)) {
+    std::cout << "file is empty of words" << std::endl;
+    ;
+  }
 
   std::vector<std::pair<std::string, size_t>> stats(word_to_count.begin(),
-                                     word_to_count.end());
+                                                    word_to_count.end());
 
   std::sort(stats.begin(), stats.end(), [](auto& lhs, auto& rhs) {
     if (lhs.second == rhs.second) {
@@ -42,10 +43,10 @@ int main() {
   });
 
   for (auto& pair : stats) {
-    std::cout << "word: " << pair.first << " is used " << pair.second << " times"
-         << std::endl;
+    std::cout << "word: " << pair.first << " is used " << pair.second
+              << " times" << std::endl;
   }
 
-  system ("pause");
+  system("pause");
   return 0;
 }
