966db2b85f9923c60b0adc4e7bfc04973db2807d
EdkyZeleny <49240143+EdkyZeleny@users.noreply.github.com>
2019-12-07 17:04:18 +0500

    clang format

--- src/lib/sample.cc
+++ src/lib/sample.cc
@@ -2,26 +2,28 @@
 
 namespace template_repository {
 
-bool pathfinder(const std::string a) { 
-    
-    std::ifstream file_stream;
-    file_stream.open(a);
+bool
+pathfinder(const std::string a)
+{
 
-    if (!file_stream.is_open()) {
+  std::ifstream file_stream;
+  file_stream.open(a);
+
+  if (!file_stream.is_open()) {
     return false;
-    }
-    else {
+  } else {
     return true;
-    }
   }
+}
 
-bool words_checker(std::map<std::string, size_t> b) { 
-    if (b.size()>0){
-        return true;
-    }
-    else {
-        return false;
-        }
- }
+bool
+words_checker(std::map<std::string, size_t> b)
+{
+  if (b.size() > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-}  // namespace qwerty
+} // namespace qwerty
