c7d3839423350476aff7d80b36b79beac1934677
EdkyZeleny <49240143+EdkyZeleny@users.noreply.github.com>
2019-12-07 17:08:59 +0500

    Update sample_unittest.cc

--- src/lib/sample_unittest.cc
+++ src/lib/sample_unittest.cc
@@ -5,16 +5,13 @@
 using template_repository::pathfinder;
 using template_repository::words_checker;
 
-TEST(SampleTest, pathfinder)
-{
+TEST(SampleTest, pathfinder) {
   EXPECT_EQ(pathfinder("rutext.txt"), true);
   EXPECT_EQ(pathfinder("0"), false);
   EXPECT_EQ(pathfinder("rutext.tx"), false);
 }
 
-TEST(SampleTest, words_checker)
-{
-
+TEST(SampleTest, words_checker) {
   std::map<std::string, size_t> k;
   ++k["слово"];
 
