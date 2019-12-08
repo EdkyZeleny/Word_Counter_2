7a25b3d43323f2ace092e318c9e98d03133b5355
EdkyZeleny <49240143+EdkyZeleny@users.noreply.github.com>
2019-12-08 16:22:38 +0500

    Update main.cc

--- src/bin/main.cc
+++ src/bin/main.cc
@@ -5,48 +5,53 @@
 using template_repository::pathfinder;
 using template_repository::words_checker;
 
-int main() {
-  setlocale(LC_CTYPE, "rus");
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
 
-  std::string path;
-  std::cout << "please, input there file source:" << std::endl;
-  std::cin >> path;
+    std::string path;
+    std::cout << "please, input there file source:" << std::endl;
+    std::cin >> path;
 
-  std::ifstream file_stream;
-  file_stream.open(path);
+    std::ifstream file_stream;
+    file_stream.open(path);
 
-  if (!pathfinder(path)) {
-    std::cout << "cannot open file. try another path." << std::endl;
-  };
+    if (!pathfinder(path))
+    {
+        std::cout << "cannot open file. try another path." << std::endl;
+        return 0;
+    };
 
-  std::map<std::string, size_t> word_to_count;
+    std::map<std::string, size_t> word_to_count;
 
-  std::string word;
+    std::string word;
 
-  while (file_stream >> word) {
-    ++word_to_count[word];
-  }
-
-  if (!words_checker(word_to_count)) {
-    std::cout << "file is empty of words" << std::endl;
-    ;
-  }
-
-  std::vector<std::pair<std::string, size_t>> stats(word_to_count.begin(),
-                                                    word_to_count.end());
-
-  std::sort(stats.begin(), stats.end(), [](auto& lhs, auto& rhs) {
-    if (lhs.second == rhs.second) {
-      return lhs.first < rhs.first;
+    while (file_stream >> word)
+    {
+        ++word_to_count[word];
     }
-    return lhs.second > rhs.second;
-  });
 
-  for (auto& pair : stats) {
-    std::cout << "word: " << pair.first << " is used " << pair.second
-              << " times" << std::endl;
-  }
+    if (!words_checker(word_to_count))
+    {
+        std::cout << "file is empty of words" << std::endl;
+        return 0;
+    }
 
-  system("pause");
-  return 0;
+    std::vector<std::pair<std::string, size_t>> stats(word_to_count.begin(), word_to_count.end());
+
+    std::sort(stats.begin(), stats.end(), [](auto& lhs, auto& rhs)
+        {
+            if (lhs.second == rhs.second)
+            {
+                return lhs.first < rhs.first;
+            }
+            return lhs.second > rhs.second;
+        });
+
+    for (auto& pair : stats)
+    {
+        std::cout << "word: " << pair.first << " is used " << pair.second << " times" << std::endl;
+    }
+
+    return 0;
 }
